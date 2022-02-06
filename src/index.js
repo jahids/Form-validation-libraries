e886c31dc745580c06d62e2cb5c293bfc99f00f2
jahids <evanjahid321@gmail.com>
2022-02-06 11:34:29 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+export default class FormValidation {
+    formElement = null
+    options = null
+
+    constructor(form, options) {
+        if (!form || !options) {
+            console.error("Invalid parameter");
+        }
+        this.formElement = form;
+        this.options = options;
+        this.formElement.addEventListener("submit", (e) => this.validateForm(e));
+    }
+
+    validateForm(e) {
+        e.preventDefault();
+        const inputNames = Object.keys(this.options)
+
+        inputNames.forEach(name => {
+            const input = this.formElement.querySelector(`[name="${name}"]`);
+
+            // input validate
+            if ('minLength' in this.options[name]) {
+                const minLength = this.options[name].minLength;
+                const nonNumeric = this.options[name].nonNumeric;
+                if (input.value.length > minLength && (/[0-9]+/).test(input.value) !== nonNumeric) {
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
+
+            //  maxlength
+            if ('maxLength' in this.options[name]) {
+                const maxLength = this.options[name].maxLength;
+                if (input.value.length > null && input.value.length < maxLength) {
+
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
+            // lowercase
+            if ('hasLowerCase' in this.options[name]) {
+                const hasLowerCase = this.options[name].hasUpperCase;
+                if ((/[a-z]/gm).test(input.value) == hasLowerCase) {
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
+
+            //  url,
+            if ('hasHttps' in this.options[name]) {
+
+                const hasHttps = this.options[name].hasHttps;
+                console.log(hasHttps);
+
+                if ((/https/g).test(input.value) == hasHttps) {
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
+
+            // HASSUPERCASE
+            if ('hasUpperCase' in this.options[name]) {
+                const hasUpperCasenew = this.options[name].hasUpperCase;
+                console.log(input.value);
+                if ((/[A-Z]/gm).test(input.value) == hasUpperCasenew) {
+
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+
+                }
+            }
+            // eror function
+            function seteror(x) {
+
+                return document.getElementById(name).innerText = `${name}  ${x}`;
+            }
+            // eror succes funtion
+            function setsuccess(x) {
+                return document.getElementById(name).innerText = `${name}  ${x}`;
+            }
+        })
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].bundle.js',
+        library: {
+            type: "var",
+            name: "testLibrary",
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+        ],
+    },
+}
+
+
+
+
